e7f5e0759bed806a022e579a65c30ed967bb947a
robertocarroll <robertocarroll@gmail.com>
2015-03-13 15:29:42 +0000

    Tweaks to the data.

--- javascripts/script.js
+++ javascripts/script.js
@@ -24,18 +24,18 @@
 					{ text:"Vampire facials" }
 				],
 				[
-					{ text:"Half naked butlers" },
+					{ text:"Half-naked butlers" },
 					{ text:"Sunglasses on head" },
 					{ text:"Diamonds" },
 					{ text:"Lauren's marriage" },
 					{ text:"Al fresco lunch" }
 				],
 				[
-					{ text:"Half naked butlers" },
-					{ text:"Sunglasses on head" },
-					{ text:"Diamonds" },
-					{ text:"Lauren's marriage" },
-					{ text:"Al fresco lunch" }
+					{ text:"Boxing" },
+					{ text:"Frozen pizza" },
+					{ text:"She poked me" },
+					{ text:"Chat in the toilets" },
+					{ text:"Tanya's book" }
 				]
 			],
 			bingo_options = {
